7e13d2ade921aee91b625442da15f7d95a06504f
Nella-01 <nella.ghanavi@gmail.com>
2024-08-24 21:27:56 +1000

    Added header and header styling.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer>
+            <p>04 1234 5678</p>
+            <p>fit.gym@gmail.com</p>
+        </footer>
+    )
+}
+
+export default Footer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,26 @@
 import React from "react"
+import "../css/styling.css"
+import navimg from "../assets/nav-bar-image.jpg"
+import profile from "../assets/profile.png"
 
 function Header() {
 
     return (
         //Profile will have dropdown: view profile, transaction history, logout for logged in user and register and login for others.
         //List items to be changed
+        <React.Fragment>
         <nav>
-            <ul className="nav-items-ul">
-                <li><a href="">Home</a></li>
-                <li><a href="">About Us</a></li>
-                <li><a href="">Classes</a></li>
-                <li><a href="">Trainers</a></li>
-                <li><a href="">Profile</a></li>
+        <img src={navimg} id="nav-image" />
+        <ul className="nav-items-ul">
+            <li className="nav-li" id="logo"><a href="">Fit Gym<br/><span id="slogan">#1 Imaginary Gym</span></a></li>
+            <li className="nav-li"><a href="">Home</a></li>
+            <li className="nav-li"><a href="">About Us</a></li>
+            <li className="nav-li"><a href="">Classes</a></li>
+            <li className="nav-li"><a href="">Trainers</a></li>
+            <li><a href=""><img src={profile} id="profile"/></a></li>
             </ul>
         </nav>
+        </React.Fragment>
     )
 }
 
