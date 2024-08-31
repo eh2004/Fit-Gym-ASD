9cb76374b84b0d13857ec0738509cadf7e8830c3
Nella-01 <nella.ghanavi@gmail.com>
2024-08-31 18:15:53 +1000

    Added profile dropdown.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,17 @@ import "../css/styling.css"
 import navimg from "../assets/nav-bar-image.jpg"
 import profile from "../assets/profile.png"
 
+
+function ProfileDropDown() {
+    return (
+        <div className="profile-dropdown">
+            <a href="">Profile</a>
+            <a href="">Transaction History</a>
+            <a href="">Logout</a>
+        </div>
+    )
+}
+
 function Header() {
 
     return (
@@ -12,15 +23,18 @@ function Header() {
         <nav>
         <img src={navimg} id="nav-image" />
         <ul className="nav-items-ul">
-            <li className="nav-li" id="logo"><a href="/index.html"><img src="/src/assets/logo.png" alt="Fit Gym Logo" id="logo-image"/></a></li>
+            <li className="nav-li" id="logo"><a href="/index.html"><img src="/src/assets/logo.png" alt="Fit Gym Logo" id="logo-image" style={{width: 80}}/></a></li>
             <li className="nav-li"><a href="/index.html">Home</a></li>
             <li className="nav-li"><a href="/src/pages/AboutUs.html">About</a></li>
             <li className="nav-li"><a href="/src/pages/Progress.html">Progress</a></li>
             <li className="nav-li"><a href="/src/pages/Leaderboard.html">Leaderboard</a></li>
             <li className="nav-li"><a href="">Classes</a></li>
             <li className="nav-li"><a href="/src/pages/Dashboard.html">Trainers</a></li>
-            <li><a href=""><img src={profile} id="profile"/></a></li>
-            </ul>
+            <div className="profile-container">
+                <li><a href=""><img src={profile} id="profile"/></a></li>
+                <ProfileDropDown />
+            </div>
+        </ul>
         </nav>
         </React.Fragment>
     )
