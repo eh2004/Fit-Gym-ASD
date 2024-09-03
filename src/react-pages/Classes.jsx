0176b3db11dad0499fc24d699af9add29eefd3f4
eh2004 <evan.a.haines@student.uts.edu.au>
2024-09-03 14:46:02 +1000

    buttons thingo

--- src/react-pages/Classes.jsx
+++ src/react-pages/Classes.jsx
@@ -7,24 +7,21 @@ import "../css/stylebest.css"
 function App() {
     return (
         <React.Fragment>
-        <Header />
-            <p class = "hometext">What would you like to book?</p>
-
-            <div class="wrapper">
-            <a href="classes.html" class="imgbox-z">
-                <div class="child bg-one">
-                <span>Classes</span>
-                </div>
-            </a>
-            <a href="dashboard.html" class="imgbox-z">
-                <div class="child bg-two">
-                <span>Trainers</span>
-                </div>
-            </a>
+            <Header />
+            <p className="hometext">What would you like to book?</p>
+            <div className="wrapper">
+                <a href="bookclass.html" className="imgbox-z">
+                    <img src="../assets/classes.jpg" alt="Classes" className="child bg-one" />
+                    <span>Classes</span>
+                </a>
+                <a href="booktrainer.html" className="imgbox-z">
+                    <img src="../assets/trainer.jpg" alt="Trainers" className="child bg-two" />
+                    <span>Trainers</span>
+                </a>
             </div>
-        <Footer />
+            <Footer />
         </React.Fragment>
-    )
+    );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
