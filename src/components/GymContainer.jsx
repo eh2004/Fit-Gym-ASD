3e61201ec7f21ee794d9c84cc6a50508c455b406
Nella-01 <nella.ghanavi@gmail.com>
2024-09-06 01:19:51 +1000

    Added hover effect to gym carousel.

--- src/components/GymContainer.jsx
+++ src/components/GymContainer.jsx
@@ -9,12 +9,14 @@ function GymContainer(props) {
     return (
         <div className="gym-container">
           <img src={props.imageLink}/>
+          <div className="gym-container-no-img">
           <h3>{props.location}</h3>
           <div className="address-container"><img src={maps_icon} style={{width: 50, height: 50}}/><a href={props.addressLink} target="_blank">{props.address}</a></div>
           <div className="number-container"><img src={phone_icon} style={{width: 35, height: 35}}/><span>{props.phoneNumber}</span></div>
           <div className="capacity-container">
             <p>Current Capacity:</p>
             <CapacityBar filled={Math.round(Math.random() * 90 + 10)}/>
+          </div>
           </div>
         </div>
     )
