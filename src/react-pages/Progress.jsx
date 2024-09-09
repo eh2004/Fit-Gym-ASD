014226a22868f54064dc63ae58bcb45cf1ea33f6
AaronNSmith <126218145+AaronNSmith@users.noreply.github.com>
2024-09-09 23:19:33 +1000

    Intial Progression Charts
    Dependencies which need to be added "npm install react-chartjs-2 chart.js"

--- src/react-pages/Progress.jsx
+++ src/react-pages/Progress.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client"
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../css/styling.css"
+import LegsLineGraph from "../components/LegsProgress";
+import UpperLineGraph from "../components/UpperProgress";
 //soon to be import progress chart 
 
 const App = () =>{
@@ -10,8 +12,13 @@ const App = () =>{
         <React.Fragment>
             <Header />
                 <div>
-                    <h1>MmmMmmmMMMMmmmmm progression</h1>
+                    <h1>Personal Progress</h1>
                 </div>
+                <div className="graphs-container">
+                    <LegsLineGraph />
+                    <UpperLineGraph />
+                </div>
+                
             <Footer />
         </React.Fragment>
     )
