83864910ab397b2b7892e8bbedf95810c08835ec
Nella-01 <nella.ghanavi@gmail.com>
2024-10-08 06:03:13 +1100

    Basic test again

--- FrontEnd/src/test/SignUp.test.js
+++ FrontEnd/src/test/SignUp.test.js
@@ -1,26 +1,18 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import SignUp from '../react-pages/SignUp.jsx';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { waitFor } from '@testing-library/dom';
+import Registration from '../components/Registration';
 
-test('Validates that first name has at least two characters', async () => {
-    render(<SignUp />);
+describe('App component', () => {
+  test('displays validation errors for empty required fields on submit', async () => {
+    render(<Registration />);
 
-    const firstNameInput = screen.getByLabelText(/First Name/i);
-
-    fireEvent.change(firstNameInput, { target: { value: 'a' } });
-
-    const submitButton = screen.getByText(/Register/i);
-    fireEvent.click(submitButton);
-
-    await waitFor(() => {
-        expect(screen.getByText(/Enter first name/i)).toBeVisible();
-    });
-
-    fireEvent.change(firstNameInput, { target: { value: 'Test' } });
+    const submitButton = screen.getByText('Register');
 
     fireEvent.click(submitButton);
 
     await waitFor(() => {
-        expect(screen.queryByText(/Enter first name/i)).not.toBeInTheDocument();
+      expect(screen.getByText('Enter first name')).toBeInTheDocument();
     });
+  });
 });
