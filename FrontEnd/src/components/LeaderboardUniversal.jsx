5c1288a72fe195dab4f0545bd60dafaee90f75c3
AaronNSmith <126218145+AaronNSmith@users.noreply.github.com>
2024-10-15 14:30:21 +1100

    Adding .env to git ignore

--- FrontEnd/src/components/LeaderboardUniversal.jsx
+++ FrontEnd/src/components/LeaderboardUniversal.jsx
@@ -1,26 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import '../css/styling.css'; // Adjust the path according to your project structure
 
-const Leaderboard = () => {
+const LeaderboardUniversal = () => {
   const [leaderboard, setLeaderboard] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/leaderboard')
+    fetch('http://localhost:3000/api/leaderboard') // Global leaderboard API
       .then(res => res.json())
-      .then(data => setLeaderboard(filterHighestLifts(data)))
+      .then(data => setLeaderboard(data))
       .catch(err => console.error('Error fetching leaderboard:', err));
   }, []);
-
-  const filterHighestLifts = (data) => {
-    const highestLifts = {};
-    data.forEach(row => {
-      const exerciseName = row.Exercise.exercise_name;
-      if (!highestLifts[exerciseName] || row.best_lift > highestLifts[exerciseName].best_lift) {
-        highestLifts[exerciseName] = row;
-      }
-    });
-    return Object.values(highestLifts);
-  };
 
   return (
     <div className="leaderboard-container">
@@ -39,7 +28,7 @@ const Leaderboard = () => {
             <tr key={index}>
               <td>{row.Exercise.exercise_name}</td>
               <td>{`${row.Workout.Customer.first_name} ${row.Workout.Customer.last_name}`}</td>
-              <td>{row.best_lift}</td>
+              <td>{row.weight}</td>
               <td>{row.reps}</td>
             </tr>
           ))}
@@ -49,4 +38,4 @@ const Leaderboard = () => {
   );
 };
 
-export default Leaderboard;
+export default LeaderboardUniversal;
