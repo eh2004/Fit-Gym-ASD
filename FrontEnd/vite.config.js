910c049c98de84a2b02ac99e9726715fb82508aa
Shin-nie <thuhangvu0302@gmail.com>
2024-11-01 15:28:59 +1100

    🌷🐹 v22 - no Publish Profile and Change Vite.config

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -6,11 +6,10 @@
 //   plugins: [react()],
 // })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',  // Use relative paths for assets
   plugins: [react()],
-  base: '/', // Ensures paths are absolute for production
-})
+});
